33099c7fc6e4daa826c3303179562b14495eee81
Hannah Engelhart <hannah.engelhart@gmail.com>
2023-07-13 20:36:29 -0500

    completed search component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,66 @@
-
+import React, {useState} from 'react';
+import { Dispatch } from 'react';
+import { useDispatch } from 'react-redux';
 
 function Search() {
+const dispatch = useDispatch();  
+const [newSearch, setNewSearch] = useState('');
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      dispatch({
+          type:'GIPH_REQUEST',
+          payload: newSearch
+      });
+      setNewSearch('');
+    }
+// let [search, setSearch] = useState({id: + 1 , name: ''})
+
   return (
     <div>
-      <input></input>
+      <form onSubmit={handleSubmit}>
+      <input value={newSearch} 
+      type='text' 
+      placeholder='giphy search'
+      onChange={event => setNewSearch(event.target.value)}
+      ></input>
+      <button type='submit'>Search</button>
+      </form>
     </div>
   )
 }
+
+export default Search;
+
+// function Feeling() {
+// //   const [newFeeling, setFeeling] = useState('');
+//   const dispatch = useDispatch();
+// //   const history = useHistory();  
+
+//   const handleSubmit = (event) => {
+//       event.preventDefault();
+//       dispatch({
+//           type:'GIPH_REQUEST',
+//           payload: category
+//       });
+//     //   setFeeling('');
+//     //   history.push('/understanding');
+//   }
+
+//   return(
+//       <div>
+//           <h1>How are you feeling today?</h1>
+//           <form onSubmit={handleSubmit}>
+//           <input
+//             type="number"
+//             value={category}
+//             placeholder="Feeling?"
+//             onChange={event => setFeeling(event.target.value)}
+//           ></input>
+//           <button type="submit">NEXT</button>
+//           </form>
+//       </div>
+//   );
+// }
+
+// export default Feeling;
--- src/state/store.jsx
+++ src/state/store.jsx
@@ -1,21 +1,42 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../../redux-intro/src/App';
+import App from '../components/App/App.jsx';
+import {takeEvery, put} from 'redux-saga/effects';
 
 const sagaMiddleware = createSagaMiddleware();
 
+function* getSearchRequest(){
+  try {
+    const response = yield fetch('/api/category');
+    const searchRequest = yield response.json();
+    yield put({type: 'GIPH_REQUEST', payload: searchRequest});
+  } catch (error) {
+    console.log('Getting search results failed:', error);
+  }
+}
+
+function searchReducer(state = '', action) {
+  if (action.type === 'GIPH_REQUEST') {
+    return action.payload;
+  }
+  return state;
+}
+
+function* watcherSaga() {
+  yield takeEvery('GIPH_REQUEST', getSearchRequest)
+}
+
 const store = createStore(
-
   combineReducers({
-
+    searchReducer: searchReducer
   }),
   applyMiddleware(sagaMiddleware, logger)
 );
 
-sagaMiddleware.run(rootSaga);
+sagaMiddleware.run(watcherSaga);
 
 export default store;
